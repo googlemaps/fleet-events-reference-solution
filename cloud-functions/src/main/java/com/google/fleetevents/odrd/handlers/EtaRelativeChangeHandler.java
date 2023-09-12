96db0c7d883f2fd114bc391795a0d12b2e2e21e2
gmalmiski <137339827+gmalmiski@users.noreply.github.com>
2023-09-12 01:28:45 -0400

    Add relative eta change handler (#50)

---
+++ cloud-functions/src/main/java/com/google/fleetevents/odrd/handlers/EtaRelativeChangeHandler.java
@@ -0,0 +1,156 @@
+package com.google.fleetevents.odrd.handlers;
+
+import com.google.cloud.Timestamp;
+import com.google.cloud.firestore.Transaction;
+import com.google.fleetevents.FleetEventHandler;
+import com.google.fleetevents.common.database.FirestoreDatabaseClient;
+import com.google.fleetevents.common.models.Change;
+import com.google.fleetevents.common.models.FleetEvent;
+import com.google.fleetevents.common.models.OutputEvent;
+import com.google.fleetevents.common.models.Pair;
+import com.google.fleetevents.common.util.TimeUtil;
+import com.google.fleetevents.odrd.models.TripFleetEvent;
+import com.google.fleetevents.odrd.models.outputs.EtaAbsoluteChangeOuputEvent;
+import com.google.fleetevents.odrd.models.outputs.EtaRelativeChangeOutputEvent;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+public class EtaRelativeChangeHandler implements FleetEventHandler {
+  private static final String RELATIVE_ETA_PAIR_KEY = "relativeEtaPair";
+  private static final double DEFAULT_THRESHOLD_PERCENT = 0.1;
+  private double thresholdPercent;
+
+  public EtaRelativeChangeHandler() {
+    this.thresholdPercent = DEFAULT_THRESHOLD_PERCENT;
+  }
+
+  @Override
+  public List<OutputEvent> handleEvent(FleetEvent fleetEvent, Transaction transaction) {
+    var tripFleetEvent = (TripFleetEvent) fleetEvent;
+    var eventTimestamp = Objects.requireNonNull(tripFleetEvent.newTrip()).getEventTimestamp();
+    var outputEvents = new ArrayList<OutputEvent>();
+
+    for (var i = 0; i < tripFleetEvent.newTripWaypoints().size(); i++) {
+      var waypointDiff = tripFleetEvent.tripWaypointDifferences().get(i);
+      var waypoint = tripFleetEvent.newTripWaypoints().get(i);
+      var etaOutputEvent =
+          tryGetRelativeEtaOutputEvent(
+              waypoint.getWaypointId(),
+              waypoint.getEventMetadata(),
+              waypointDiff,
+              waypoint.getEta(),
+              eventTimestamp,
+              fleetEvent,
+              false);
+      etaOutputEvent.ifPresent(outputEvents::add);
+    }
+    var newTrip = tripFleetEvent.newTrip();
+    var etaOutputEvent =
+        tryGetRelativeEtaOutputEvent(
+            newTrip.getTripId(),
+            newTrip.getEventMetadata(),
+            tripFleetEvent.tripDifferences(),
+            newTrip.getEta(),
+            eventTimestamp,
+            fleetEvent,
+            true);
+    etaOutputEvent.ifPresent(outputEvents::add);
+
+    return outputEvents;
+  }
+
+  @Override
+  public boolean respondsTo(
+      FleetEvent fleetEvent,
+      Transaction transaction,
+      FirestoreDatabaseClient firestoreDatabaseClient) {
+    if (fleetEvent instanceof TripFleetEvent tripFleetEvent) {
+      return tripFleetEvent.tripDifferences().containsKey("eta")
+          || tripFleetEvent.tripWaypointDifferences().stream()
+              .map(differences -> differences.containsKey("eta"))
+              .reduce(false, Boolean::logicalOr);
+    }
+    return false;
+  }
+
+  @Override
+  public boolean verifyOutput(OutputEvent outputEvent) {
+    return outputEvent instanceof EtaAbsoluteChangeOuputEvent;
+  }
+
+  @SuppressWarnings("unchecked")
+  public Optional<OutputEvent> tryGetRelativeEtaOutputEvent(
+      String identifier,
+      Map<String, Object> eventMetadata,
+      Map<String, Change> differences,
+      Timestamp newEta,
+      Timestamp eventTimestamp,
+      FleetEvent fleetEvent,
+      boolean isTripOutputEvent) {
+    var outputEvent = Optional.<OutputEvent>empty();
+    if (!differences.containsKey("eta")) {
+      return outputEvent;
+    }
+
+    var hasRelativeEtaPair = eventMetadata.containsKey(RELATIVE_ETA_PAIR_KEY);
+    if (hasRelativeEtaPair) {
+      var relativeEtaPair = (Pair<Long, Timestamp>) eventMetadata.get(RELATIVE_ETA_PAIR_KEY);
+      var originalDurationMilliseconds = relativeEtaPair.getKey();
+      var originalEta = relativeEtaPair.getValue();
+      double etaDelta =
+          TimeUtil.timestampDifferenceMillis(newEta.toSqlTimestamp(), originalEta.toSqlTimestamp());
+      var percentDurationChange = Math.abs(etaDelta / originalDurationMilliseconds);
+      outputEvent =
+          checkRelativeEta(
+              percentDurationChange,
+              identifier,
+              originalEta,
+              newEta,
+              eventTimestamp,
+              originalDurationMilliseconds,
+              fleetEvent,
+              isTripOutputEvent);
+    } else {
+      var originalEta = (Timestamp) differences.get("eta").newValue;
+      var originalDurationMilliseconds =
+          TimeUtil.timestampDifferenceMillis(
+              originalEta.toSqlTimestamp(), eventTimestamp.toSqlTimestamp());
+      if (originalDurationMilliseconds <= 0) {
+        // If the duration is non-positive use a dummy value of 1 second (1000 milliseconds)
+        originalDurationMilliseconds = 1000;
+      }
+      eventMetadata.put(
+          RELATIVE_ETA_PAIR_KEY, new Pair<>(originalDurationMilliseconds, originalEta));
+    }
+    return outputEvent;
+  }
+
+  Optional<OutputEvent> checkRelativeEta(
+      double percentDurationChange,
+      String identifier,
+      Timestamp originalEta,
+      Timestamp newEta,
+      Timestamp eventTimestamp,
+      Long originalDurationMilliseconds,
+      FleetEvent fleetEvent,
+      boolean isTripOutputEvent) {
+    if (percentDurationChange < thresholdPercent) {
+      return Optional.empty();
+    }
+    var etaOutputEvent = new EtaRelativeChangeOutputEvent();
+    etaOutputEvent.setIdentifier(identifier);
+    etaOutputEvent.setOriginalEta(originalEta);
+    etaOutputEvent.setNewEta(newEta);
+    etaOutputEvent.setOriginalDuration(originalDurationMilliseconds);
+    etaOutputEvent.setPercentDurationChange(percentDurationChange);
+    etaOutputEvent.setThresholdPercent(thresholdPercent);
+    etaOutputEvent.setEventTimestamp(eventTimestamp);
+    etaOutputEvent.setFleetEvent(fleetEvent);
+    etaOutputEvent.setIsTripOutputEvent(isTripOutputEvent);
+
+    return Optional.of(etaOutputEvent);
+  }
+}
---
+++ cloud-functions/src/main/java/com/google/fleetevents/odrd/models/outputs/EtaRelativeChangeOutputEvent.java
@@ -0,0 +1,133 @@
+package com.google.fleetevents.odrd.models.outputs;
+
+import com.google.cloud.Timestamp;
+import com.google.fleetevents.common.models.OutputEvent;
+import java.util.Objects;
+
+public class EtaRelativeChangeOutputEvent extends OutputEvent {
+
+  private Timestamp originalEta;
+  private Long originalDuration;
+  private Timestamp newEta;
+
+  private double percentDurationChange;
+  private double thresholdPercent;
+
+  private String identifier;
+
+  private boolean isTripOutputEvent;
+
+  private Timestamp eventTimestamp;
+
+  public EtaRelativeChangeOutputEvent() {
+    type = Type.RELATIVE_ETA;
+  }
+
+  public Timestamp getOriginalEta() {
+    return originalEta;
+  }
+
+  public void setOriginalEta(Timestamp originalEta) {
+    this.originalEta = originalEta;
+  }
+
+  public Long getOriginalDuration() {
+    return originalDuration;
+  }
+
+  public void setOriginalDuration(Long originalDurationMilliseconds) {
+    this.originalDuration = originalDurationMilliseconds;
+  }
+
+  public double getPercentDurationChange() {
+    return percentDurationChange;
+  }
+
+  public void setPercentDurationChange(double percentDurationChange) {
+    this.percentDurationChange = percentDurationChange;
+  }
+
+  public Timestamp getNewEta() {
+    return newEta;
+  }
+
+  public void setNewEta(Timestamp newEta) {
+    this.newEta = newEta;
+  }
+
+  public double getThresholdPercent() {
+    return thresholdPercent;
+  }
+
+  public void setThresholdPercent(double thresholdPercent) {
+    this.thresholdPercent = thresholdPercent;
+  }
+
+  public String getIdentifier() {
+    return identifier;
+  }
+
+  public void setIdentifier(String identifier) {
+    this.identifier = identifier;
+  }
+
+  public boolean getIsTripOutputEvent() {
+    return isTripOutputEvent;
+  }
+
+  @Override
+  public String toString() {
+    return "EtaRelativeChangeOutputEvent{"
+        + "originalEta="
+        + originalEta
+        + ", originalDuration="
+        + originalDuration
+        + ", newEta="
+        + newEta
+        + ", percentDurationChange="
+        + percentDurationChange
+        + ", thresholdPercent="
+        + thresholdPercent
+        + ", identifier='"
+        + identifier
+        + '\''
+        + ", isTripOutputEvent="
+        + isTripOutputEvent
+        + ", eventTimestamp="
+        + eventTimestamp
+        + ", fleetEvent="
+        + null
+        + ", type="
+        + type
+        + '}';
+  }
+
+  public void setIsTripOutputEvent(boolean tripOutputEvent) {
+    isTripOutputEvent = tripOutputEvent;
+  }
+
+  public Timestamp getEventTimestamp() {
+    return eventTimestamp;
+  }
+
+  public void setEventTimestamp(Timestamp eventTimestamp) {
+    this.eventTimestamp = eventTimestamp;
+  }
+
+  @Override
+  public boolean equals(Object object) {
+    if (object instanceof EtaRelativeChangeOutputEvent that) {
+      return Objects.equals(this.fleetEvent, that.fleetEvent)
+          && Objects.equals(this.newEta, that.newEta)
+          && Objects.equals(this.originalDuration, that.originalDuration)
+          && Objects.equals(this.originalEta, that.originalEta)
+          && Objects.equals(this.percentDurationChange, that.percentDurationChange)
+          && Objects.equals(this.thresholdPercent, that.thresholdPercent)
+          && Objects.equals(this.identifier, that.identifier)
+          && Objects.equals(this.isTripOutputEvent, that.isTripOutputEvent)
+          && Objects.equals(this.eventTimestamp, that.eventTimestamp)
+          && Objects.equals(this.type, that.type);
+    }
+    return false;
+  }
+}
